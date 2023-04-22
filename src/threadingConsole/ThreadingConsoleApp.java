45389eab752fae9df83576282fa8eba6a763200a
skochauf <stephan.kochauf@lv.campus02.at>
2023-04-22 08:44:57 +0200

    Console Printer

---
+++ src/threadingConsole/ThreadingConsoleApp.java
@@ -0,0 +1,28 @@
+package threadingConsole;
+
+import java.io.IOException;
+
+public class ThreadingConsoleApp {
+
+    public static void main(String[] args) {
+        DateTimePrinterBackgroundWorker backgroundWorker = new DateTimePrinterBackgroundWorker();
+
+        Thread th = new Thread(backgroundWorker);
+        th.start();
+        //backgroundWorker.run(); ACHTUNG: Startet keinen Thread
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            // Warten auf ein Zeichen
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // Zeichen eingelesen
+        backgroundWorker.setShouldRun(false);
+    }
+}
